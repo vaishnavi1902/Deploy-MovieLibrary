2805ea5e5da4dac80456858e9fc1a6b6a7bf2952
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2024-06-02 18:34:35 +0530

    Delete j directory

--- j/backend/routes/movieRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { searchMovies } = require('../controllers/movieController');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/search', protect, searchMovies);
-
-module.exports = router;
